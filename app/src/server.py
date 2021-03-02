4515e689e80983d78ae9b410dc5c6c7c54bfdea3
willianvalerio <willianvalerio@outlook.com>
2021-03-02 11:48:53 -0300

    Add docker-compose and otel confif

--- app/src/server.py
+++ app/src/server.py
@@ -5,7 +5,7 @@ app = Sanic(name="otel-sanic")
 
 @app.route('/')
 async def test(request):
-    return json({'hello': 'world1'})
+    return json({'hello': 'world'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000,auto_reload=True)
