d95cec58cb435752fe79b030cb2702e6d3319f5a
willianvalerio <willianvalerio@outlook.com>
2021-03-03 10:30:01 -0300

    Change code and configurations to send data through Datadog agent

--- datadog-agent/app/src/server.py
+++ datadog-agent/app/src/server.py
@@ -1,29 +1,56 @@
 from sanic import Sanic
 from sanic.response import json
 
-from opentelemetry import trace,baggage
-from opentelemetry.exporter.otlp.trace_exporter import OTLPSpanExporter
-from opentelemetry.sdk.resources import Resource
+from opentelemetry import trace
+from opentelemetry.exporter.datadog import (
+    DatadogExportSpanProcessor,
+    DatadogSpanExporter,
+)
+from opentelemetry.exporter.datadog.propagator import DatadogFormat
+#from opentelemetry.propagate import get_global_textmap, set_global_textmap
+from opentelemetry.propagators.composite import CompositeHTTPPropagator
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchExportSpanProcessor
+from opentelemetry.sdk.resources import Resource
+
 import os
 
 service_name = os.getenv('DD_SERVICE')
-otel_collector = os.getenv('OTEL_EXPORTER_OTLP_ENDPOINT')
+dd_agent = os.getenv('DD_AGENT_HOST')
 app = Sanic(name=service_name)
 
-resource = Resource.create({"service.name": service_name})
 
+resource = Resource.create({"service.name": service_name})
 trace.set_tracer_provider(TracerProvider(resource=resource))
+
+trace.get_tracer_provider().add_span_processor(
+    DatadogExportSpanProcessor(
+        DatadogSpanExporter(
+            agent_url="http://"+dd_agent+":8126", service=service_name
+        )
+    )
+)
+
+# global_textmap = get_global_textmap()
+# if isinstance(global_textmap, CompositeHTTPPropagator) and not any(
+#     isinstance(p, DatadogFormat) for p in global_textmap._propagators
+# ):
+#     set_global_textmap(
+#         CompositeHTTPPropagator(
+#             global_textmap._propagators + [DatadogFormat()]
+#         )
+#     )
+# else:
+#     set_global_textmap(DatadogFormat())
+
 tracer = trace.get_tracer(__name__)
-otlp_exporter = OTLPSpanExporter(endpoint=otel_collector, insecure=True)
-span_processor = BatchExportSpanProcessor(otlp_exporter)
-trace.get_tracer_provider().add_span_processor(span_processor)
 
 @app.route('/')
 async def test(request):
+    param = request.args.get("param")
     with tracer.start_as_current_span("hello-world"):
-        return json({'hello': 'world'})
+        if param == "error":
+            raise ValueError("forced server error")
+        return json({'hello': param})
 
 if __name__ == '__main__':
     with tracer.start_as_current_span("start"):
